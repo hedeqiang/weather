3baeed267458f2c922424f0de3c45a1558766947
hedeqiang <laravel_code@163.com>
2018-09-26 10:37:03 +0000

    Add travis ci config file.

--- src/Weather.php
+++ src/Weather.php
@@ -15,7 +15,7 @@ class Weather
     protected $key;
     protected $guzzleOptions = [];
 
-    public function __construct(string $key)
+    public function __construct($key)
     {
         $this->key = $key;
     }
@@ -63,7 +63,7 @@ class Weather
      * @throws HttpException
      * @throws InvalidArgumentException
      */
-    public function getWeather($city, string $type = 'base', string $format = 'json')
+    public function getWeather($city, $type = 'base', string $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
